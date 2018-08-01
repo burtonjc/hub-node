25e11d113c7fae95a0f960c46b65922c11208124
Jacob Burton <burtonjc35@gmail.com>
2018-08-01 00:44:38 -0600

    handle missing configstore

--- src/helpers/github.ts
+++ src/helpers/github.ts
@@ -1,4 +1,4 @@
-import * as inquirer from 'inquirer';
+import chalk from 'chalk';
 import * as request from 'request-promise-native';
 
 import { askForGitHubOTP } from './inquirer';
@@ -34,9 +34,40 @@ export const createAuthorization = async (credentials: ICredentials, otp?: strin
       const { otp } = await askForGitHubOTP();
 
       return createAuthorization(credentials, otp);
+    } else if (error.statusCode === 422) {
+      await deleteAuthorization(credentials, otp);
+
+      return createAuthorization(credentials, otp);
     } else {
       return Promise.reject(error);
     }
   }
+}
 
+const deleteAuthorization = async (credentials: ICredentials, otp?: string) => {
+  const authorizations = await request.get('https://api.github.com/authorizations', {
+    auth: credentials,
+    headers: otp ? getHeaders({ 'X-GitHub-OTP': otp }) : getHeaders(),
+    json: {
+      note: 'hub-node: CLI for GitHub written in NodeJS and available from NPM',
+      note_url: 'https://github.com/burtonjc/hub-node',
+      scopes: 'repo',
+    }
+  });
+
+  const authorization = (authorizations as any[]).find((authorization) =>
+    (authorization.note as string).startsWith(`${pkg.name}: `)
+  );
+
+  if (!authorization) return Promise.reject('Failed to remove old authorization');
+
+  await request.delete(authorization.url, {
+    auth: credentials,
+    headers: otp ? getHeaders({ 'X-GitHub-OTP': otp }) : getHeaders(),
+    json: {
+      note: 'hub-node: CLI for GitHub written in NodeJS and available from NPM',
+      note_url: 'https://github.com/burtonjc/hub-node',
+      scopes: 'repo',
+    }
+  });
 }
